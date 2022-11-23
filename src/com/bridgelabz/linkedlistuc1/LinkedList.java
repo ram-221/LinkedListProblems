6b0db0d07cccb756bc66a8080a840f1db0263061
ram-221 <rameshmalli857.com>
2022-11-23 20:12:39 +0530

    Creat a Simple Linked List of 56,30 and 70

---
+++ src/com/bridgelabz/linkedlistuc1/LinkedList.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.linkedlistuc1;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(30);
+		Node thirdNode = new Node(70);
+
+		Node head = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+
+		Node temp = head;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+}
